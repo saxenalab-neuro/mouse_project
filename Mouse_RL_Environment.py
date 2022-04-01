8ad841bae602e899643f515d2ab0fdd9cb115adf
Andrea Chacon <87379754+AndreaC02@users.noreply.github.com>
2022-04-01 13:13:45 -0400

    starting environment

--- Mouse_RL_Environment.py
+++ Mouse_RL_Environment.py
@@ -9,9 +9,51 @@ import pybullet as p
 import pybullet_envs
 import yaml
 
+arm_indexes = [104, 105, 106, 107, 108, 110, 111, 112]
+#RShoulder_rotation - 104
+#RShoulder_adduction - 105
+#RShoulder_flexion - 106
+#RElbow_flexion - 107
+#RElbow_supination - 108
+#RWrist_adduction - 110
+#RWrist_flexion - 111
+#RMetacarpus1_flextion - 112, use link (carpus) for pos
+
+#helper functions, possibly to be added to a util file
+def cart2sph(x, y, z):
+    hxy = np.hypot(x, y)
+    r = np.hypot(hxy, z) #rho
+    el = np.arctan2(z, hxy) #theta
+    az = np.arctan2(y, x) #phi
+    return az, el, r
+
+def sph2cart(az, el, r):
+    rcos_theta = r * np.cos(el)
+    x = rcos_theta * np.cos(az)
+    y = rcos_theta * np.sin(az)
+    z = r * np.sin(el)
+    return x, y, z
 
 class Mouse_Env(gym.Env):
-    pose_file = "/Users/andreachacon/mouse_biomechanics_paper/data/config/default_pose.yaml"
+
+    def __init__(self, model_path, frame_skip, mouseId):
+        self.frame_skip= frame_skip
+        self.model = mouseId 
+
+        #using point in space: theta, rho, phi
+        theta = np.pi #FIGURE OUT STARTING POSITION
+        rho = 0 #FIGURE OUT STARTING POSITION
+        phi = 0 #FIGURE OUT STARTING POSITION
+        target_pos = [theta, rho, phi]
+
+        #Meta parameters for the simulation
+        self.n_fixedsteps= 20
+        self.timestep_limit= (1319 * 1) + self.n_fixedsteps
+        # self._max_episode_steps= self.timestep_limit/ 2
+        self._max_episode_steps= 1000   #Do not matter. It is being set in the main.py where the total number of steps are being changed.
+        self.threshold = .03
+
+    pose_file = "/files/default_pose.yaml"
     def resetPosition(self, pose_file): 
         joint_list = []
         for joint in range(p.getNumJoints(self, )):
@@ -34,21 +76,99 @@ class Mouse_Env(gym.Env):
         self.resetPosition()
         return self
 
-    #def reward():
-    #    x
-        # get xyz of hand, NOT JOINT
-        # compare with target XYZ
-        # check threshold
-        # ex reward: r_x= 1/(1000**d_x)
-        # get cost if distance is greater than threshold 
+    def reward(self): 
+        hand_pos = p.getLinkState(self.model, 112)[0] #(x, y, z)
+        x, y, z= sph2cart(self.target_pos[0], self.target_pos[1], self.target_pos[2])
+
+        d_x = np.abs(hand_pos[0] - x)
+        d_y = np.abs(hand_pos[1] - y)
+        d_z = np.abs(hand_pos[2] - z)
+
+        if d_x > self.threshold or d_y > self.threshold or d_z > self.threshold:
+            return -5
+
+
+        #can play around with these functions later
+        r_x= 1/(1000**d_x)
+        r_y= 1/(1000**d_y)
+        r_z= 1/(1000**d_z)
+
+        reward= r_x + r_y + r_z
+
+        return reward
+
+    def is_done(self):
+        x, y, z= sph2cart(self.target_pos[0], self.target_pos[1], self.target_pos[2])
+        target = [x, y, z]
+        hand_pos =hand_pos = p.getLinkState(self.model, 112)[0] #(x, y, z)
+        criteria= hand_pos - target
+
+        if self.istep < self.timestep_limit:
+            if np.abs(criteria[0]) > self.threshold or np.abs(criteria[1]) > self.threshold or np.abs(criteria[2]) > self.threshold:
+                return True
+            else:
+                return False
+        else:
+            return True
+
+    def update_target_pos(self):
+        #depends on how fast we want it to move, play around with values
+        if(self.target_pos[0] < self.max_theta):
+            self.target_pos[0] += np.pi/6
+        else:
+            self.target_pos[0] -= np.pi/6
+
+        if(self.target_pos[1] < self.max_rho):
+                self.target_pos[1] += np.pi/6
+        else:
+            self.target_pos[1] -= np.pi/6
+
+        if(self.target_pos[2] < self.max_phi):
+                self.target_pos[0] += np.pi/6
+        else:
+            self.target_pos[2] -= np.pi/6
+    
+    def update_joints(self):
+        for x in arm_indexes:
+            p.setJointMotorControl(self.model, x, p.TORQUE_CONTROL, force = .002)
+            #THIS IS TEMPORARY, NEED TO FIGURE OUT WHAT WORKS
 
 
 
-    #def step():
-    #    x
+    def step(self, action):
+        self.istep += 1
+
+        #can edit threshold with episodes
+        if self.istep > self.n_fixedsteps:
+            self.threshold = 0.032
+
+        self.do_simulation(action, self.frame_skip)
+
+        reward= self.reward()
+
+        #can play around with cost/reward value
+        cost= self.get_cost(action)
+        final_reward= (5*reward) - (0.5*cost)
+
+        self.update_target_pos()
+        self.update_joints()
+
+        done= self.is_done()
+        
+
+        return final_reward, done
         #get reward
         #update target position
         #update joints
         #check steps, etc.
+        #need to figure out how to edit torque here
     
-    
+
+#TO_DO:
+# probably need to add camera 
+# add super class !!!
+# add initialization of neural networks when written
+# play with threshold
+# add maximums for circular motion
+# write do_simulation-- how to move it?
+# need action space and observation space
