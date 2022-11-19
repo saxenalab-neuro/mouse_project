7ee4e37068e0eb4674c922fde9d504b548c05c3b
JohnLazzari <lazzarijohn2@gmail.com>
2022-11-19 14:51:27 -0500

    cca

--- scripts/cca.py
+++ scripts/cca.py
@@ -5,29 +5,53 @@ from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression, Ridge
 from sklearn.metrics import r2_score
+import seaborn as sns
+from sklearn.decomposition import PCA
+
+sns.set_style('darkgrid')
+colors = sns.color_palette()
 
 #Load the experimental activities
 
-exp_alt1 = loadmat('/Users/malmani/Downloads/mean_firing_rate_alt1.mat')['cell_mean_firing_rate'][0, 1]
+exp_alt1 = loadmat('../data/mean_firing_rate_alt1.mat')['cell_mean_firing_rate'][0, 1]
 
-exp_alt_slow = loadmat('/Users/malmani/Downloads/mean_firing_rate_alt_slow.mat')['cell_mean_firing_rate'][0, 1]
+exp_alt_slow = loadmat('../data/mean_firing_rate_alt_slow.mat')['cell_mean_firing_rate'][0, 1]
 
-exp_alt_fast = loadmat('/Users/malmani/Downloads/mean_firing_rate_alt_fast.mat')['cell_mean_firing_rate'][0, 1]
+exp_alt_fast = loadmat('../data/mean_firing_rate_alt_fast.mat')['cell_mean_firing_rate'][0, 1]
 
-exp_alt1= exp_alt1[:, 225:405].T
-exp_alt_slow= exp_alt_slow[:, 255:475].T
-exp_alt_fast= exp_alt_fast[:, 230:400].T
+exp_alt1= exp_alt1[:, 230:406].T
+exp_alt_slow= exp_alt_slow[:, 256:476].T
+exp_alt_fast= exp_alt_fast[:, 239:401].T
 
 #Now load the lstm activities
 #dim= [#timepoints, neurons]
-agent_alt1= np.load()
-agent_slow= np.load()
-agent_fast= np.load()
+agent_alt1 = np.loadtxt('mouse_1_activity.txt')
+agent_alt1 = agent_alt1[1:]
+
+agent_slow = np.loadtxt('mouse_slow_activity.txt')
+
+agent_fast = np.loadtxt('mouse_fast_activity.txt')
+agent_fast = agent_fast[1:]
 
 #Now do the CCA
 A_exp = exp_alt1
 A_agent = agent_alt1
 
+PC_plot = PCA(n_components=3)
+principalComponents_1 = PC_plot.fit_transform(agent_alt1)
+
+
+principalComponents_fast = PC_plot.fit_transform(agent_fast)
+
+
+principalComponents_slow = PC_plot.fit_transform(agent_slow)
+
+scatter = plt.scatter(principalComponents_slow[:, 0], principalComponents_slow[:, 1], c=principalComponents_slow[:, 2])
+cbr = plt.colorbar(scatter)
+cbr.set_label('PC 3')
+plt.ylabel('PC 2')
+plt.xlabel('PC 1')
+plt.savefig('pcs_slow.png')
 # PC_agent = PCA(n_components= 10)
 PC_exp = PCA(n_components= 10)
 #
@@ -53,12 +77,11 @@ for k in range(10):
         plt.plot(A_exp[:, 9 - k]/np.max(A_exp[:,9-k]) + k * 4, linewidth=1.5, c='k')
 
 plt.ylabel('Reconstructed M1 Population Activity', size=14)
-plt.xticks([0, 500], ['0', '0.5'], size= 14)
+plt.xticks([0, 177], ['0', '0.5'], size= 14)
 plt.yticks([])
 # plt.legend()
 # plt.savefig('/Users/malmani/Creative Cloud Files/Fig3/CCA_619.svg', format='svg', dpi=300, transparent= True)
-plt.show()
-
+plt.savefig('cca_1.png')
 
 print('Now printing the correlations')
 
